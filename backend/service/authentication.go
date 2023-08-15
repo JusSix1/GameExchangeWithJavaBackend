c31aed54df5125a36e45d23e3add25aa64989186
JusSix1 <Patcharapolwor28112544@gmail.com>
2023-08-15 19:54:10 +0700

    s

--- backend/service/authentication.go
+++ backend/service/authentication.go
@@ -26,11 +26,6 @@ type JwtWrapperAdmin struct {
 	ExpirationHours int64
 }
 
-type JwtClaimAdmin struct {
-	Email string
-	jwt.StandardClaims
-}
-
 // Generate Token generates a jwt token
 func (j *JwtWrapperUser) GenerateTokenUser(email string) (signedToken string, err error) {
 	claims := &JwtClaimUser{
@@ -66,55 +61,6 @@ func (j *JwtWrapperUser) ValidateTokenUser(signedToken string) (claims *JwtClaim
 	}
 
 	claims, ok := token.Claims.(*JwtClaimUser)
-	if !ok {
-		err = errors.New("Couldn't parse claims")
-		return
-	}
-
-	if claims.ExpiresAt < time.Now().Local().Unix() {
-		err = errors.New("JWT is expired")
-		return
-	}
-
-	return
-
-}
-
-// Generate Token generates a jwt token
-func (j *JwtWrapperAdmin) GenerateTokenAdmin(email string) (signedToken string, err error) {
-	claims := &JwtClaimAdmin{
-		Email: email,
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(j.ExpirationHours)).Unix(),
-			Issuer:    j.Issuer,
-		},
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
-	signedToken, err = token.SignedString([]byte(j.SecretKey))
-	if err != nil {
-		return
-	}
-
-	return
-}
-
-// Validate Token validates the jwt token // ยืนยันว่าหมดเวลารึยัง
-func (j *JwtWrapperAdmin) ValidateTokenAdmin(signedToken string) (claims *JwtClaimAdmin, err error) {
-	token, err := jwt.ParseWithClaims(
-		signedToken,
-		&JwtClaimAdmin{},
-		func(token *jwt.Token) (interface{}, error) {
-			return []byte(j.SecretKey), nil
-		},
-	)
-
-	if err != nil {
-		return
-	}
-
-	claims, ok := token.Claims.(*JwtClaimAdmin)
 	if !ok {
 		err = errors.New("Couldn't parse claims")
 		return
